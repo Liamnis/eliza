d91b9669eca6c2d5952ede7f02d9abeb8d2b8bdc
Liamnis <liam.nisbet@epitech.digital>
2023-03-28 10:30:52 +0200

    Push

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,107 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="chatbot-container">
+    <div class="chatbot-messages">
+      <div v-for="message in messages" :key="message.id" class="chatbot-message">
+        <p v-if="message.type === 'bot'" class="chatbot-bot-message">{{ message.text }}</p>
+        <p v-else class="chatbot-user-message">{{ message.text }}</p>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="chatbot-input">
+      <input type="text" v-model="currentMessage" @keydown.enter="sendMessage">
+      <button @click="sendMessage">Envoyer</button>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import { create } from '@openai/api';
+
+export default {
+  name: 'Chatbot',
+  data() {
+    return {
+      messages: [],
+      currentMessage: ''
+    }
+  },
+  methods: {
+    async sendMessage() {
+      const message = {
+        type: 'user',
+        text: this.currentMessage
+      };
+      this.messages.push(message);
+      this.currentMessage = '';
+
+      const openai = create({
+        apiKey: 'VOTRE_CLE_API_DE_CHATGPT',
+        model: 'text-davinci-002',
+        engine: 'davinci',
+        prompt: `Conversation avec un utilisateur : ${message.text}`,
+        maxTokens: 150
+      });
+
+      const response = await openai.complete({});
+      const botMessage = {
+        type: 'bot',
+        text: response.choices[0].text
+      };
+      this.messages.push(botMessage);
+    }
+  }
+}
+</script>
+
+<style>
+.chatbot-container {
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+  width: 300px;
+  border: 1px solid black;
+  border-radius: 5px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.chatbot-messages {
+  flex-grow: 1;
+  overflow-y: auto;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.chatbot-message {
+  padding: 5px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.chatbot-bot-message {
+  background-color: #eee;
+  border-radius: 5px;
+  margin-bottom: 5px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.chatbot-user-message {
+  background-color: #2196F3;
+  color: white;
+  border-radius: 5px;
+  margin-bottom: 5px;
+}
+
+.chatbot-input {
+  display: flex;
+}
+
+.chatbot-input input {
+  flex-grow: 1;
+  border: none;
+  border-radius: 5px;
+  padding: 5px;
+}
+
+.chatbot-input button {
+  border: none;
+  border-radius: 5px;
+  background-color: #2196F3;
+  color: white;
+  padding: 5px 10px;
+  margin-left: 5px;
+  cursor: pointer;
 }
 </style>
